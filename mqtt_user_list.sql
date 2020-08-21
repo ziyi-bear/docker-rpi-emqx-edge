bcc95bf63e53e1d70dfb2512689269c0376d5099
ziyi-bear <m0724001@gm.nuu.edu.tw>
2020-08-22 03:28:36 +0800

    預設三使用者

--- mqtt_user_list.sql
+++ mqtt_user_list.sql
@@ -1,3 +1,4 @@
 INSERT INTO `mqtt_user` (`id`, `username`, `password`, `salt`, `is_superuser`, `created`) VALUES
-(1,	'm0724001',	'efa1f375d76194fa51a3556a97e641e61685f914d446979da50a551a4333ffd7',	NULL,	0,	NULL),
-(2,	'ys1815yc',	'a68a8c361dc429920889ef569931e8a80b7028e531fc33361ba3cc324cb47756',	'whitebear',	0,	NULL);
+(1,	'm0724001',	'c3a6ab89a38e4b32be44a3398e17feb6caca45dab8996f382ad32ed4da988e2c',	'bear',	0,	NULL),
+(2,	'ys1815yc',	'a68a8c361dc429920889ef569931e8a80b7028e531fc33361ba3cc324cb47756',	'whitebear',	0,	NULL),
+(3,	'test',	'bfd0448b9e1ec4e2d03a21e3e662190275d5574880c8d9bc93082151b8bbc36e',	'bear',	0,	NULL);
