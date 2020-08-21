354f1f362459a3b36c933be31631174e3f6748f4
ziyi-bear <m0724001@gm.nuu.edu.tw>
2020-08-22 00:29:14 +0800

    新增人員名單

--- mqtt_user_list.sql
+++ mqtt_user_list.sql
@@ -1,2 +1,3 @@
 INSERT INTO `mqtt_user` (`id`, `username`, `password`, `salt`, `is_superuser`, `created`) VALUES
-(1,	'm0724001',	'efa1f375d76194fa51a3556a97e641e61685f914d446979da50a551a4333ffd7',	NULL,	0,	NULL);
+(1,	'm0724001',	'efa1f375d76194fa51a3556a97e641e61685f914d446979da50a551a4333ffd7',	NULL,	0,	NULL),
+(2,	'ys1815yc',	'a68a8c361dc429920889ef569931e8a80b7028e531fc33361ba3cc324cb47756',	'whitebear',	0,	NULL);
